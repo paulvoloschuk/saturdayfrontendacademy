64f28496880d49f05c92b2d98d1b9bf98741830c
paulvoloschuk <paul.voloschuk@gmail.com>
2017-02-08 15:55:37 +0200

    Checkers action update

--- homework3.1/script.js
+++ homework3.1/script.js
@@ -13,10 +13,12 @@ function Checkers(global_container){
 
   this.players = Array();
   this.field = Array();
+  this.cells = Array();
   this.current_figures = Array();
   this.current_actions = Array();
   this.current_select = false;
   this.current_player = 0;
+  this.current_side = 'light';
 
   // Constructor
   this.Construct = function(){
@@ -42,8 +44,8 @@ function Checkers(global_container){
       if(pass_cell) x = 0;
       else x = 1;
       this.field[y] = Array();
-      if(y < 3) side = 'light';
-      if(y > 4) side = 'dark';
+      if(y < 3) side = 'dark';
+      if(y > 4) side = 'light';
       for (var x = 0; x <= this.size_x; x++) {
         this.field[y][x] = new Cell(id_cell++);
         if(side) {
@@ -62,48 +64,82 @@ function Checkers(global_container){
   this.GenerateField = function(){
     this.Debug('Generating field & figures');
     var HTML_field = '', HTML_figures = '', HTML_tooltip = '';
-    for(var y = 0; y <= this.size_y; y++) {
+    for(var id = 0, y = 0; y <= this.size_y; y++) {
       HTML_field += '<div data-id="' + y + '" class="row">';
       for(var x = 0; x <= this.size_x; x++) {
         if(x == 0 || x == this.size_x) HTML_tooltip = '<span class="n-tooltip">' + (y + 1) + '</span>';
         if(y == 0 || y == this.size_y) HTML_tooltip += '<span class="s-tooltip">' + String.fromCharCode(65 + x) + '</span>';
-        HTML_field += '<div class="cell" data-x="' + x + '" data-y="' + y + '">'  + HTML_tooltip + '</div>';
+        HTML_field += '<div class="cell" id="cell_' + (id++) + '" data-x="' + x + '" data-y="' + y + '">'  + HTML_tooltip + '</div>';
         HTML_tooltip = '';
         HTML_figures += '<div class="' + this.field[y][x].value.type + ' ' + this.field[y][x].value.side + '" style="' + this.FigurePositioning(x, y) + '"></div>';
       }
       HTML_field += '</div>';
+      this.cells = document.getElementsByClassName('cell');
     }
     this.global_container.className = 'player' + this.current_player;
     this.global_container.innerHTML = '<p>' + this.players[0].name + '</p><p>' + this.players[1].name + '</p><div id="figures">' + HTML_figures + '</div><div id="field">' + HTML_field + '</div>';
-    this.Debug('Setting click event');
+    this.Debug('Setting click events');
     var cells = document.getElementsByClassName('cell');
     for (var i = 0; i < cells.length; i++) {
-      cells[i].onclick = this.ClickEvent;
+      cells[i].onclick = function(e) {Checkers.ClickEvent(e.target);}
     }
   }
 
   // Rebuilt field
-  this.RenewField = function(){
-    this.Debug('Rebuilding field');
+  this.UpdateField = function(){
+    this.Debug('Updating field');
     for (var y = 0; y <= this.size_y; y++) {
       for (var x = 0; x <= this.size_x; x++) {
-        // lol
+        // Action firstly
+        if(this.field[y][x].action) this.cells[this.field[y][x].id].className = 'cell ' + this.field[y][x].action.name;
+        else this.cells[this.field[y][x].id].className = 'cell';
       }
     }
+    // console.log(this.field);
   }
 
   this.FigurePositioning = function(x, y){
     return 'top:' + (y * 8) + 'vmin; left:' + (x * 8) + 'vmin;';
   }
+  this.PossibleActions = function(x, y){
 
-  this.ClickEvent = function(){
-    Checkers.Debug('Click on x:' + this.attributes[1].value + ' y:' + this.attributes[2].value);
-    if(!Checkers.current_select){
+  }
+
+  this.ClearActions = function(){
+    this.Debug('Abbadon all actions');
+    this.current_actions = Array();
+    this.current_select = false;
+    for (var y = 0; y <= this.size_y; y++) {
+      for (var x = 0; x <= this.size_x; x++) {
+        this.field[y][x].action = false;
+      }
+    }
+  }
+
+  this.ClickEvent = function(target){
+    var recursive = false;
+    var cell_x = target.attributes[2].value
+    var cell_y = target.attributes[3].value;
+    this.Debug('Click on x:' + cell_x + ' y:' + cell_y);
+    if(!this.current_select){
       // Вход в режим выбора действий, выход при нажатии туда-же.
       // Далее расчет всех возможных ходов.
+      console.log(this.field[cell_y][cell_x].value.side);
+      if(this.field[cell_y][cell_x].value.side == this.current_side){
+        this.current_select = true;
+        this.field[cell_y][cell_x].action = new Action(0, 'select');
+        this.current_actions.push(this.field[cell_y][cell_x].action);
+        this.PossibleActions(cell_x, cell_y);
+      }
     } else {
-      // Выполнение хода.
+      // Выполнение хода
+      if(this.field[cell_y][cell_x].action){
+        alert(1); //--------------------------------------ЗАКОНЧИТЬ
+      }else this.ClearActions();
+      if(this.field[cell_y][cell_x].value) recursive = this.ClickEvent(target);
     }
+    if(!recursive)this.UpdateField();
+    return true;
   }
 
   this.Construct();
@@ -126,7 +162,7 @@ function Cell(id, value = false){
 }
 function Action(id, name){
   this.id = id;
-  this.name = name;
+  this.name = name; // move,atack,select
   this.cordinates = Array();
   this.kill = Array();
 }
