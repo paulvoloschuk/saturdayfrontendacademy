0ace509267f878471de9d640f4cc837ced349479
paulvoloschuk <paul.voloschuk@gmail.com>
2017-02-11 02:06:30 +0200

    H2 3.2 complete

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 2. 28.01.17 [FormulaCounting+AgeSwitch+Calculator](https://paulvoloschuk.github.io/saturdayfrontendacademy/homework2.1/index.html)
 3. 28.01.17 [Responsitive+Flexbox](https://paulvoloschuk.github.io/saturdayfrontendacademy/homework2.2/index.html)
 4. 04.02.17 [Checkers & Agony](https://paulvoloschuk.github.io/saturdayfrontendacademy/homework3.1/index.html)
+5. 04.02.17 [ArraySort+ArrayReverse+MatrixGeneration](https://paulvoloschuk.github.io/saturdayfrontendacademy/homework3.2/index.html)
--- homework3.2/script.js
+++ homework3.2/script.js
@@ -28,8 +28,9 @@ function Exercise1(){
   var result_container = document.getElementById('ex1_result');
   var inputs = document.getElementsByClassName('cell');
   var arr = [];
-  for(var i = 0; i < inputs.length; i++)
-    arr[i] = '<span class="code"><span>' + i + '</span>:' + inputs[i].value + '</span>';
+  for(var i = 0; i < inputs.length; i++){
+    arr[i] = '<span class="code"><span>' + i + '</span>:' + ((inputs[i].value == '') ? '<span class="warn">empty</span>' : inputs[i].value) + '</span>';
+  }
   result_container.innerHTML = Reverse(arr).join(' ');
 }
 
@@ -57,11 +58,124 @@ function Exercise2(){
   result2_container.innerHTML = arr_random.join(' ');
 }
 
+function Exercise3(){
+  this.target = document.getElementById('matrix_function');
+  this.figure;
+  this.size = 10;
+  this.color = [];
+  this.color_inputs = [];
+  this.matrix = [];
+
+  this.GenerateField = function(){
+    var cells = [];
+    var HTML = '';
+    for (var y = 0; y < this.size; y++) {
+      HTML += '<div class="matrix_row">';
+      for (var x = 0; x < this.size; x++) {
+        HTML += '<div id="cell_' + x + '' + y + '" class="matrix_cell"></div>';
+      }
+      HTML += '</div>';
+    }
+    document.getElementById('ex3_result').innerHTML = HTML;
+    this.RecountColor();
+  }
+
+  this.ClearMatrix = function(){
+    for (var y = 0; y < this.size; y++) {
+      this.matrix[y] = [];
+      for (var x = 0; x < this.size; x++) {
+        this.matrix[y][x] = 'transparent';
+      }
+    }
+  }
+
+  this.RecountColor = function(){
+    this.ClearMatrix();
+    this.figure = this.target.elements.font.value;
+    this.color_inputs = [this.target[3], this.target[4], this.target[5]];
+    this.color = [this.color_inputs[0].value, this.color_inputs[1].value, this.color_inputs[2].value];
+    for (var i = 0; i < this.color_inputs.length; i++) this.color_inputs[i].style.cssText = 'background-color:#' + this.color[i];
+    switch (this.figure) {
+      case 'kaleidoscope':
+        this.DrawCross();
+        break;
+      case 'square':
+        for (var i = 0; i < Random(4 ,this.size-4); i++) {
+          this.DrawSquare([Random(-this.size ,this.size),Random(-this.size+4 ,this.size-4)], Random(2, this.size-3), this.color[Random(0,2)], this.color[Random(0,2)]);
+        }
+        break;
+      case 'wave':
+        this.DrawWave();
+        break;
+    }
+
+    this.Paint();
+  }
+
+  this.Paint = function(){
+    for (var y = 0; y < this.size; y++) {
+      for (var x = 0; x < this.size; x++) {
+        document.getElementById('cell_' + x + '' + y).style.cssText = 'background-color:' + ((this.matrix[y][x] == 'transparent') ? this.matrix[y][x] : '#' + this.matrix[y][x]);
+      }
+    }
+  }
+
+  this.DrawSquare = function(start_index, size, color, fill = 'transparent'){
+    for (var y = start_index[0]; y < (start_index[0] + size); y++) {
+      for (var x = start_index[1]; x < (start_index[1] + size); x++) {
+        if(y >= 0 && y < this.size && x >= 0 && x < this.size){
+          if(y == start_index[0] || y == start_index[0] + size - 1 || x == start_index[1] || x == start_index[1] + size - 1)
+            this.matrix[y][x] = color;
+          else this.matrix[y][x] = fill;
+        }
+      }
+    }
+  }
+
+  this.DrawCross = function(){
+    var temp_color;
+    for (var y = 0; y < this.size; y++) {
+      for (var x = 0; x < this.size; x++) {
+        if(x < this.size/2 && y < this.size/2){
+          temp_color = this.color[Random(0 ,2)];
+          this.matrix[y][x] =
+          this.matrix[x][y] =
+          this.matrix[y][this.size - x - 1] =
+          this.matrix[this.size - x - 1][y] =
+          this.matrix[x][this.size - y - 1] =
+          this.matrix[this.size - y - 1][x] =
+          this.matrix[this.size - x - 1][this.size - y - 1] =
+          this.matrix[this.size - y - 1][this.size - x - 1] =
+          temp_color;
+        }
+      }
+    }
+  }
+
+  this.DrawWave = function(){
+    var temp_color;
+    for (var z = 0; z < this.size*2; z++) {
+      temp_color = this.color[Random(0 ,2)];
+      for (var y = 0, x = z; y < this.size, x >= 0; y++, x--) {
+        // for (var x = z; x >= 0; x--) {
+          if(y >= 0 && y < this.size && x >= 0 && x < this.size) this.matrix[y][x] = temp_color;
+        // }
+      }
+
+    }
+  }
+
+
+  this.GenerateField();
+}
 
 // Initialize exercises
 Exercise1();
 Exercise2();
+var Matrix = new Exercise3();
 
 // Events
 document.getElementById('reverse_function').onkeyup = function(e){Exercise1()};
 document.getElementById('sort_function').onsubmit = function(e){Exercise2();return false;};
+document.getElementById('matrix_function').onchange = function(){Matrix.RecountColor();};
+document.getElementById('matrix_function').onsubmit = function(){Matrix.RecountColor();return false;};
